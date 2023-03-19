3a0c296c33881fba8b984900a7ec5a8ed58cf4b8
Bret-B <64377038+Bret-B@users.noreply.github.com>
2023-03-18 20:18:46 -0700

    Experimental blender import script changes
    Script changes to enable backface culling for transparent materials for the Cycles renderer.

--- scripts/blender_fix_textures.py
+++ scripts/blender_fix_textures.py
@@ -3,15 +3,33 @@
 
 import bpy
 
+backface_culling = True  # optional
+cycles = True
+
 for mat in bpy.data.materials:
     if not mat.node_tree:
         continue
 
-    # mat.use_backface_culling = True  # optional
+    mat.use_backface_culling = backface_culling
     for node in mat.node_tree.nodes:
         if node.type == 'BSDF_PRINCIPLED':
             if node.inputs["Alpha"].is_linked:
                 mat.blend_method = 'HASHED'
                 mat.shadow_method = 'HASHED'
+                
+                if cycles and backface_culling:
+                    mix_shader = mat.node_tree.nodes.new('ShaderNodeMixShader')
+                    mix_shader.location = (250, 500)
+                    geometry = mat.node_tree.nodes.new('ShaderNodeNewGeometry')
+                    geometry.location = (0, 700)
+                    transparent = mat.node_tree.nodes.new('ShaderNodeBsdfTransparent')
+                    transparent.location = (0, 430)
+                
+                    mat.node_tree.links.remove(mat.node_tree.nodes['Material Output'].inputs[0].links[0])
+                    mat.node_tree.links.new(geometry.outputs['Backfacing'], mix_shader.inputs['Fac'])
+                    mat.node_tree.links.new(transparent.outputs[0], mix_shader.inputs[2])
+                    mat.node_tree.links.new(node.outputs[0], mix_shader.inputs[1])
+                    mat.node_tree.links.new(mix_shader.outputs[0], mat.node_tree.nodes['Material Output'].inputs[0])
+                    
         elif node.type == 'TEX_IMAGE':
             node.interpolation = 'Closest'
