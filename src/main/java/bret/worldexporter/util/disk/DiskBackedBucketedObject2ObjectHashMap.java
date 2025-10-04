c737eef34e9d6a36c4f378de6f44fb27eea46ba8
Bret-B <64377038+Bret-B@users.noreply.github.com>
2025-10-04 00:38:56 -0500

    Replace hashmap implementation that was slow in certain cases

---
+++ src/main/java/bret/worldexporter/util/disk/DiskBackedBucketedObject2ObjectHashMap.java
@@ -0,0 +1,49 @@
+package bret.worldexporter.util.disk;
+
+import java.io.Serializable;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.function.Function;
+
+public class DiskBackedBucketedObject2ObjectHashMap<T extends Serializable, V extends Serializable> extends UsesBuckets<HashMap<T, V>> {
+    private static final String SUBDIR = "object2intmap";
+    private final DiskBackedBuckets<HashMap<T, V>> buckets;
+    private final Function<T, Long> bucketFunction;
+
+    public DiskBackedBucketedObject2ObjectHashMap(int bucketCacheSize,
+                                                  String baseCacheDir,
+                                                  Function<T, Long> bucketFunction) {
+        buckets = new DiskBackedBuckets<>(
+                bucketCacheSize,
+                Paths.get(baseCacheDir, SUBDIR).toString(),
+                HashMap::new);
+        this.bucketFunction = bucketFunction;
+    }
+
+    public V get(T key) {
+        long bucketKey = bucketFunction.apply(key);
+        HashMap<T, V> bucket = buckets.getBucket(bucketKey);
+        return bucket.get(key);
+    }
+
+    public void put(T key, V value) {
+        long bucketKey = bucketFunction.apply(key);
+        HashMap<T, V> bucket = buckets.getBucket(bucketKey, true);
+        bucket.put(key, value);
+        buckets.setDirty(bucketKey);
+    }
+
+    public boolean containsKey(T key) {
+        long bucketKey = bucketFunction.apply(key);
+        HashMap<T, V> bucket = buckets.getBucket(bucketKey);
+        return bucket != null && bucket.containsKey(key);
+    }
+
+    public boolean isEmpty() {
+        return buckets.isEmpty();
+    }
+
+    public void clear() {
+        buckets.clear();
+    }
+}
